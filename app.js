a30e221dd4adb590a07c1d1b8339641ceb867f20
merritske <merritske@gmail.com>
2022-11-02 11:46:27 +0100

    eerste versie mobile

---
+++ app.js
@@ -0,0 +1,8 @@
+let logo = document.getElementById('logo')
+let nav = document.querySelector('.nav')
+
+logo.addEventListener('click', ()=>{
+    console.log('yep')
+ nav.style.display = 'block'
+})
+
